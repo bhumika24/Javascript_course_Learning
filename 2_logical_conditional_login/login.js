e78ecc86b5ebe1259369a4419b19536bbb22446b
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-02 12:43:35 +0530

    session 2 logical conditional login in javascript

---
+++ 2_logical_conditional_login/login.js
@@ -0,0 +1,12 @@
+//allow user to access course if he is:
+//logged in from email
+//logged in from google
+//logged in from facebook
+
+var email = true;
+var facebook = false;
+var google = false;
+
+ if(email || facebook || google) {
+     console.log("login success");
+ }
