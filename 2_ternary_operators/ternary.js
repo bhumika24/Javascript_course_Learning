2152cc7ca578f2e46f4ccd5cf0293133f89ee754
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-02 13:24:50 +0530

    session 2 ternary operators in javascript

---
+++ 2_ternary_operators/ternary.js
@@ -0,0 +1,13 @@
+// //hear we add sign out feature wen after user logged in
+// //show user a sign out button if he is authenticated.
+// //other wise show signin or loggin button
+
+var authenticated = false/true;
+
+// if(authenticated) {
+//     console.log("show sign out button");
+// }else {
+//     console.log("show login option");
+// }
+
+authenticated ? console.log("signOut Button") : console.log("LOGIN");
