2a6c61a8e02fb768a1a24ea26c05e33d0617f873
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-04 17:05:42 +0530

    hear we get to know how scope chain will work in javascript

---
+++ 3_scope_chaining_javascript/scope.js
@@ -0,0 +1,22 @@
+var name = "babe";
+console.log("Line numbe 2",name);
+
+function sayName(){
+    
+    var name = "mrs k";
+     console.log("Line number 6", name);
+    satNameTwo();
+
+function satNameTwo(){
+       
+    var name = "mr k";
+        console.log("Line number 9",name);
+    }
+}
+
+
+
+
+
+
+sayName();
