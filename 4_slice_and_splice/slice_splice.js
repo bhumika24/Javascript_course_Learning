2f25b49c5f16f4c754c8c12d3c7c62a715d251ff
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-11 15:55:12 +0530

    slice and splice

---
+++ 4_slice_and_splice/slice_splice.js
@@ -0,0 +1,6 @@
+var users = ["tom","ted","john"," sor","pin","sod","pink"];
+//console.log(user.slice(3,5));
+
+//splice
+users.splice(1,3,"babe");//hear 1 to 3 index number get deleted and replaced by babe.
+console.log(users);
