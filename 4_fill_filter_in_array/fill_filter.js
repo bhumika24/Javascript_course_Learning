ba31e6e3cc7adc00d6eb5d5af0284b0065128623
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-11 15:41:17 +0530

    arrow function and callback function, how it works with return key and without return by using ()

---
+++ 4_fill_filter_in_array/fill_filter.js
@@ -0,0 +1,11 @@
+//fill and filter methods
+ var testArray = [2,4,6,8,1,3,5,7];
+ console.log(testArry.fill("h", 2 ,5)); //"h" says tht to fill in array all h and 2 says which is the starting postion of h to fill in array all h to fill in array
+     //says the start and ebnd of array to replace 2,5
+
+
+//filter out
+const myNumber = [23,24,25,88,99,55,98]
+const result = myNumber.filter((num) => num!=55); //(num) => num <55 does filter which are less it does not show in out put
+
+console.log(result);//
