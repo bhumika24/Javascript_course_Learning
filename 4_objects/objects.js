924928bb496513206e4e41ac22f0e2871a6c6670
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-12 11:48:11 +0530

    how ti initize objects in javascript and how to update the values of key

---
+++ 4_objects/objects.js
@@ -0,0 +1,15 @@
+var user = {
+    firstName : "john",
+    lastName : "k",
+    role : "Admin",
+    loginCount : 32,
+    facebookSignedIn: true,
+};
+
+//console.log(user.firstName);
+//console.table(user);
+
+//update the values 
+
+user.role = "administartion";
+console.log(user.role);
