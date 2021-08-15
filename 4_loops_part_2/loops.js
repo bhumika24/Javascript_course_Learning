7a933ccb0d44181b87e9baaa52b158f034fd50a7
bhumibabe <bhumika.sudhakar@gmail.com>
2021-08-15 13:33:36 +0530

    foreach loop syntax

---
+++ 4_loops_part_2/loops.js
@@ -0,0 +1,28 @@
+const myState = [
+    "katnataka",
+    "goa",
+    "delhi",
+    1996,
+    "ap",
+    "kerala",
+    "tamil Nadu",
+];
+
+// var i = 0;
+// for(;;) {
+//     if(i>3) break;
+//     console.log(i);
+//     i++;
+// }
+// for each loop
+myState.forEach((s)  => (console.log(s)) )     //is a callback function insted of running i=0;i++ conduction
+
+
+
+
+
+
+
+
+
+
